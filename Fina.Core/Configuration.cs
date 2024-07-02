0f014cfb4927389b55c17157907ea3b1b7388129
Pedro Lucas <pedrogmobile2@gmail.com>
2024-07-02 20:57:00 -0300

    feat(core): adds responses configurations

--- Fina.Core/Configuration.cs
+++ Fina.Core/Configuration.cs
@@ -1,7 +1,10 @@
-﻿namespace Fina.Core;
+﻿using System.Net;
+
+namespace Fina.Core;
 
 public class Configuration
 {
     public const int DefaultPageNumber = 1;
     public const int DefaultPageSize = 30;
+    public const int DefaultStatusCode = (int)HttpStatusCode.OK;
 }
