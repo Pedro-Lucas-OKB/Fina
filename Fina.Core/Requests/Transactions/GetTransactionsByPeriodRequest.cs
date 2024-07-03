49edb8357cc2ba57a4f7159ce1a6a054898cd534
Pedro Lucas <pedrogmobile2@gmail.com>
2024-07-02 21:11:18 -0300

    feat(core): adds handlers configurations

--- Fina.Core/Requests/Transactions/GetTransactionsByPeriodRequest.cs
+++ Fina.Core/Requests/Transactions/GetTransactionsByPeriodRequest.cs
@@ -1,6 +1,6 @@
 ﻿namespace Fina.Core.Requests.Transactions;
 
-public class GetTransactionsByPeriod : Request
+public class GetTransactionsByPeriodRequest : Request
 {
     public DateTime? StartDate { get; set; }
     public DateTime? EndDate { get; set; }
