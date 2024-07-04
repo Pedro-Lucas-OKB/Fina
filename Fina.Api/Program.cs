84cd46812a7ee78b6703296d467303744cce4af7
Pedro Lucas <pedrogmobile2@gmail.com>
2024-07-04 00:08:36 -0300

    feat(api): adds DbContext configurations

--- Fina.Api/Program.cs
+++ Fina.Api/Program.cs
@@ -1,4 +1,13 @@
+using Fina.Api.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
+
+const string connectionString = "Server=localhost,1434;Database=fina;User ID=sa;Password=1q2w3e4r@#$;Trusted_Connection=False;TrustServerCertificate=True;";
+
+builder.Services.AddDbContext<AppDbContext>(
+    options => options.UseSqlServer(connectionString));
+
 var app = builder.Build();
 
 app.MapGet("/", () => "Hello World!");
